adc4503f961002a6a21efcc96b5079cfe4d480a3
Daniel Quevedo <danielelvisquevedo@gmail.com>
2025-06-25 19:51:04 -0300

    feat: Minor fix and remove unused dependency

--- src/main/java/com/processvideoapi/adapters/controllers/PaymentController.java
+++ src/main/java/com/processvideoapi/adapters/controllers/PaymentController.java
@@ -1,11 +1,8 @@
 package com.processvideoapi.adapters.controllers;
 
 import com.processvideoapi.core.domain.Payment;
-import com.processvideoapi.core.domain.dto.PaymentNotificationDto;
-import com.processvideoapi.core.domain.enumeration.PaymentStatus;
 import com.processvideoapi.core.ports.usecases.Payment.CreatePaymentUseCasePort;
 import com.processvideoapi.core.ports.usecases.Payment.GetPaymentByOrderUseCasePort;
-import com.processvideoapi.core.ports.usecases.Payment.PaymentWebhookUseCasePort;
 import com.processvideoapi.core.ports.usecases.Payment.UpdatePaymentStatusUseCasePort;
 
 import java.util.List;
@@ -23,7 +20,7 @@ public class PaymentController {
     public Payment createPayment(Payment payment){
         return createPaymentUseCasePort.createPayment(payment);
     }
-//
+
     public List<Payment> getPaymentByOrderId(Long orderId){
         return getPaymentByOrderUseCasePort.getPaymentByOrderId(orderId);
     }
