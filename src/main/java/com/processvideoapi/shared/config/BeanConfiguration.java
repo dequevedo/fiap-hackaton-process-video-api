09c6f4208a44cb7b3f28fbc1c0eb7bcb4684f919
Daniel Quevedo <danielelvisquevedo@gmail.com>
2025-06-26 19:13:40 -0300

    feat: Add Localstack

--- src/main/java/com/processvideoapi/shared/config/BeanConfiguration.java
+++ src/main/java/com/processvideoapi/shared/config/BeanConfiguration.java
@@ -6,9 +6,14 @@ import com.processvideoapi.core.ports.usecases.Payment.ProcessVideoUseCasePort;
 import com.processvideoapi.core.usecases.Payment.ProcessVideoUseCase;
 import com.processvideoapi.frameworks.jpa.JpaOrderRepositoryAdapter;
 import com.processvideoapi.frameworks.jpa.JpaVideoRepository;
+import io.awspring.cloud.autoconfigure.AwsClientCustomizer;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
+import software.amazon.awssdk.services.s3.S3Configuration;
+
+import java.net.URI;
 
 @Configuration
 public class BeanConfiguration {
@@ -25,6 +30,14 @@ public class BeanConfiguration {
     @Bean
     public ProcessVideoUseCasePort createOrderUseCasePort(VideoGatewayPort videoGatewayPort) {
         return new ProcessVideoUseCase(videoGatewayPort);
+    }
+
+    @Bean
+    public AwsClientCustomizer<S3ClientBuilder> s3Customizer() {
+        return builder -> builder
+                .endpointOverride(URI.create("http://localhost:4566"))
+                .serviceConfiguration(
+                        S3Configuration.builder().pathStyleAccessEnabled(true).build());
     }
 
 }
